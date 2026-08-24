51b2c44676b15fc943e21a0f634514fea7ef35a7
Divyendra Rajawat <divyendra.rajawat7@gmail.com>
2026-08-24 19:08:02 +0530

    Update q2.c

--- Day_12/q2.c
+++ Day_12/q2.c
@@ -9,4 +9,20 @@ Above at ₹12/unit*/
 
 int main() {
     int units;
+    float bill;
+    printf("Enter the number of units consumed: ");
+    scanf("%d", &units);
+
+    if (units <= 100) {
+        bill = units * 5;
+    } else if (units <= 200) {
+        bill = (100 * 5) + ((units - 100) * 7);
+    } else if (units <= 300) {
+        bill = (100 * 5) + (100 * 7) + ((units - 200) * 10);
+    } else {
+        bill = (100 * 5) + (100 * 7) + (100 * 10) + ((units - 300) * 12);
+    }
+
+    printf("The electricity bill for %d units consumed is: ₹%.2f\n", units, bill);
+    return 0;
 }
