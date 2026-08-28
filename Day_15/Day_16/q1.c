a31fd75a9844cb6ebfd7b0aa21988bee48df05a2
Divyendra Rajawat <divyendra.rajawat7@gmail.com>
2026-08-28 09:12:39 +0530

    Add files via upload

---
+++ Day_15/Day_16/q1.c
@@ -0,0 +1,33 @@
+// A program to print the  binary equivalent of a number 
+
+#include <stdio.h>
+
+int main(){
+	int n, binary[32], i = 0;
+
+	printf("Enter a number: ");
+	scanf("%d", &n);
+
+	// Special case for 0
+	if(n == 0){
+		printf("Binary = 0\n");
+		return 0;
+	}
+
+	// Convert binary into decimal 
+	while(n >0){
+		binary[i] = n % 2;
+		n = n / 2;
+		i ++;
+	}
+
+	// Print the binary in reverse order 
+	printf("Binary = ");
+	for(int j = i - 1; j >= 0; j --){
+		printf("%d", binary[j]);
+	}
+
+	printf("\n");
+
+	return 0;
+}
