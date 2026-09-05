183e1c466ee1226e1c0165adff2bc4251318e31c
Divyendra Rajawat <divyendra.rajawat7@gmail.com>
2026-09-05 19:11:28 +0530

    Add files via upload

---
+++ Day_24/q1.c
@@ -0,0 +1,12 @@
+// A program to print the following pattern 
+
+#include <stdio.h>
+
+int main(){
+    for(int i = 1; i <= 5; i++){
+        for(int j = 1; j <= i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
